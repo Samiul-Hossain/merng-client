9ece8e34a030d2575db06bb6f1129c603e7bd307
Samiul <samiulhossain1337@gmail.com>
2021-06-07 08:02:06 +0600

    Minor Fixes

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,20 +4,20 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-import {setContext} from 'apollo-link-context'
+import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://guarded-inlet-41054.herokuapp.com/",
 });
 
-const authLink = setContext(()=>{
-  const token = localStorage.getItem('jwtToken');
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
   return {
     headers: {
-      Authorization: token? `Bearer ${token}` : ''
-    }
-  }
-})
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
